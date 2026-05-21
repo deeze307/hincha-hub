bc71706a0b5d693d11d733847c6e6258b1d28ad5
Diego Maidana <dmaidana@cstechlab.com>
2026-05-20 21:13:13 -0300

    quita "fecha siguiente"

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -19,7 +19,7 @@ export default function HomePage() {
 
       {/* Fila 1 */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-12 gap-4 md:gap-5">
-        <NextMatchCard />
+        {/* <NextMatchCard /> */}
         <MatchesCard />
         <MyTournamentsCard />
         <RecentActivityCard />
