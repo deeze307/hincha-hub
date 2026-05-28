96c33200e7d8ba74427f1108b26b725c61ae39ab
Diego Maidana <dmaidana@cstechlab.com>
2026-05-28 15:03:35 -0300

    ajustes de imagenes

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         background: '#09090F',
         fit: 'contain',
       },
-      padding: 0.3,
+      padding: 0.38,
     },
     apple: {
       sizes: [180],
